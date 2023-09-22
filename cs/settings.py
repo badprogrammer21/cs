414defc275461093dd17faa76e28ce7e69def38b
badprogrammer21 <yyyy05016@gmail.com>
2023-09-22 22:47:44 +0600

    removed email

--- cs/settings.py
+++ cs/settings.py
@@ -160,13 +160,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'recoveryscienceandart01@gmail.com'
-EMAIL_HOST_PASSWORD = 'bfav xkqr uvmd fxdb'
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
