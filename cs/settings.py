6228304e23b55f1244846d4e540da17a6887adaf
badprogrammer21 <yyyy05016@gmail.com>
2023-09-22 22:41:59 +0600

    added localsettings

--- cs/settings.py
+++ cs/settings.py
@@ -170,3 +170,8 @@ EMAIL_HOST_PASSWORD = 'bfav xkqr uvmd fxdb'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
